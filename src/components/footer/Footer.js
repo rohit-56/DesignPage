4f89be1f80ce9ded14267aaf39f31e717920ad86
Rohit <rohit.sharma2@hexaviewtech.com>
2022-06-30 10:15:34 +0530

    footer of page

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h5>Copyright © 2022 Hexaview Technologies, Inc.</h5>
+    </div>
+  );
+}
+
+export default Footer;
